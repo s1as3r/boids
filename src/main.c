d60a78faf59ea5f9c50036e91c2d87d167d52148
Arbaaz Shafiq <arbaazshafiq@gmail.com>
2026-02-07 20:26:00 +0530

    move definitions past main in main.c

--- src/main.c
+++ src/main.c
@@ -8,7 +8,6 @@
 #include "base.h"
 
 #include "boid.c"
-#include "boid.h"
 #include "rand.c"
 
 #include <raylib.h>
@@ -20,72 +19,10 @@
 #include <cimgui.h>
 // clang-format on
 
-Color _imcolor_to_rl(const ImVec4_c *color) {
-  return (Color){
-      .r = (u8)(color->x * 255),
-      .g = (u8)(color->y * 255),
-      .b = (u8)(color->z * 255),
-      .a = (u8)(color->w * 255),
-  };
-}
-
-ImVec4_c _rlcolor_to_im(const Color *color) {
-  return (ImVec4_c){
-      .x = ((f32)color->r / 255.0f),
-      .y = ((f32)color->g / 255.0f),
-      .z = ((f32)color->b / 255.0f),
-      .w = ((f32)color->a / 255.0f),
-  };
-}
-
-void draw_ui_single_flock(Flock *flock) {
-  if (igTreeNode_Str("Debug Draw##dbg")) {
-    igCheckbox("Enable##dbg", &flock->debug_draw);
-    if (!flock->debug_draw) {
-      igBeginDisabled(true);
-    }
-    igCheckbox("Protected Radius##dbg", &flock->debug_protected);
-    igCheckbox("Visual Radius##dbg", &flock->debug_visual);
-    igCheckbox("Env Edges##dbg", &flock->debug_env_edge);
-    igCheckbox("Velocity##dbg", &flock->debug_velocity);
-    if (!flock->debug_draw) {
-      igEndDisabled();
-    }
-    igSeparator();
-    igTreePop();
-  }
-  ImVec4_c color = _rlcolor_to_im(&flock->color);
-  igColorEdit3("Color", (float *)&color, ImGuiColorEditFlags_None);
-  flock->color = _imcolor_to_rl(&color);
-
-  igSliderFloat("Protected Radius", &flock->protected_radius, 0.0f,
-                flock->visual_radius, "%.2f", ImGuiSliderFlags_None);
-  igSliderFloat("Visual Radius", &flock->visual_radius, 0.0f, 400.0f, "%.2f",
-                ImGuiSliderFlags_None);
-  igSliderFloat("Avoid Factor", &flock->avoid_factor, 0.0f, 2.0f, "%.2f",
-                ImGuiSliderFlags_None);
-  igSliderFloat("Matching Factor", &flock->matching_factor, 0.0f,
-                flock->avoid_factor, "%.2f", ImGuiSliderFlags_None);
-  igSliderFloat("Centering Factor", &flock->centering_factor, 0.0f, 2.0f,
-                "%.2f", ImGuiSliderFlags_None);
-  igSliderFloat("Turn Factor", &flock->turn_factor, 5.0f, 100.0f, "%.2f",
-                ImGuiSliderFlags_None);
-  igSliderFloat("Min Speed", &flock->min_speed, 10.0f, flock->max_speed, "%.2f",
-                ImGuiSliderFlags_None);
-  igSliderFloat("Max Speed", &flock->max_speed, 100.0f, 500.0f, "%.2f",
-                ImGuiSliderFlags_None);
-  igSeparator();
-}
-
-void draw_ui(Flock *flocks, u64 n) {
-  Flock *flock;
-  for (u64 i = 0; i < n; i++) {
-    flock = &flocks[i];
-    igBegin(TextFormat("Flock %d", i), NULL, ImGuiWindowFlags_AlwaysAutoResize);
-    draw_ui_single_flock(flock);
-    igEnd();
-  }
-}
+Color _imcolor_to_rl(const ImVec4_c *color);
+ImVec4_c _rlcolor_to_im(const Color *color);
+void draw_ui_single_flock(Flock *flock);
+void draw_ui(Flock *flocks, u64 n);
 
 i32 main(void) {
   const i32 screen_w = 1600, screen_h = 1200;
@@ -159,4 +96,71 @@ i32 main(void) {
   UnloadRenderTexture(target);
   CloseWindow();
   return 0;
+}
+
+Color _imcolor_to_rl(const ImVec4_c *color) {
+  return (Color){
+      .r = (u8)(color->x * 255),
+      .g = (u8)(color->y * 255),
+      .b = (u8)(color->z * 255),
+      .a = (u8)(color->w * 255),
+  };
+}
+
+ImVec4_c _rlcolor_to_im(const Color *color) {
+  return (ImVec4_c){
+      .x = ((f32)color->r / 255.0f),
+      .y = ((f32)color->g / 255.0f),
+      .z = ((f32)color->b / 255.0f),
+      .w = ((f32)color->a / 255.0f),
+  };
+}
+
+void draw_ui_single_flock(Flock *flock) {
+  if (igTreeNode_Str("Debug Draw##dbg")) {
+    igCheckbox("Enable##dbg", &flock->debug_draw);
+    if (!flock->debug_draw) {
+      igBeginDisabled(true);
+    }
+    igCheckbox("Protected Radius##dbg", &flock->debug_protected);
+    igCheckbox("Visual Radius##dbg", &flock->debug_visual);
+    igCheckbox("Env Edges##dbg", &flock->debug_env_edge);
+    igCheckbox("Velocity##dbg", &flock->debug_velocity);
+    if (!flock->debug_draw) {
+      igEndDisabled();
+    }
+    igSeparator();
+    igTreePop();
+  }
+  ImVec4_c color = _rlcolor_to_im(&flock->color);
+  igColorEdit3("Color", (float *)&color, ImGuiColorEditFlags_None);
+  flock->color = _imcolor_to_rl(&color);
+
+  igSliderFloat("Protected Radius", &flock->protected_radius, 0.0f,
+                flock->visual_radius, "%.2f", ImGuiSliderFlags_None);
+  igSliderFloat("Visual Radius", &flock->visual_radius, 0.0f, 400.0f, "%.2f",
+                ImGuiSliderFlags_None);
+  igSliderFloat("Avoid Factor", &flock->avoid_factor, 0.0f, 2.0f, "%.2f",
+                ImGuiSliderFlags_None);
+  igSliderFloat("Matching Factor", &flock->matching_factor, 0.0f,
+                flock->avoid_factor, "%.2f", ImGuiSliderFlags_None);
+  igSliderFloat("Centering Factor", &flock->centering_factor, 0.0f, 2.0f,
+                "%.2f", ImGuiSliderFlags_None);
+  igSliderFloat("Turn Factor", &flock->turn_factor, 5.0f, 100.0f, "%.2f",
+                ImGuiSliderFlags_None);
+  igSliderFloat("Min Speed", &flock->min_speed, 10.0f, flock->max_speed, "%.2f",
+                ImGuiSliderFlags_None);
+  igSliderFloat("Max Speed", &flock->max_speed, 100.0f, 500.0f, "%.2f",
+                ImGuiSliderFlags_None);
+  igSeparator();
+}
+
+void draw_ui(Flock *flocks, u64 n) {
+  Flock *flock;
+  for (u64 i = 0; i < n; i++) {
+    flock = &flocks[i];
+    igBegin(TextFormat("Flock %d", i), NULL, ImGuiWindowFlags_AlwaysAutoResize);
+    draw_ui_single_flock(flock);
+    igEnd();
+  }
 }
